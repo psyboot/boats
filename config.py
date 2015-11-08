f43fc2ee616036b051082251781395b0e783faa7
Nick Talov <ntalov@yandex.ru>
2015-11-08 23:41:51 +0300

    Database skeleton: create app.db database, model for Boats,Users tables, migration, creation database scripts REAL

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 CSRF_ENABLED = True
 SECRET_KEY = "tnt"
 MONGO_DB_IP = "127.0.0.1"
